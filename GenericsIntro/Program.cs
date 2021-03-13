5f30daf6ba3bba8b5b463dab77d468b0aa9a27bf
Yakup İlyas Çağan <yakupilyascagan@gmail.com>
2021-03-13 16:07:43 +0300

    13 Mart Ders Sonu

--- GenericsIntro/Program.cs
+++ GenericsIntro/Program.cs
@@ -6,6 +6,9 @@ namespace GenericsIntro
     {
         static void Main(string[] args)
         {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Engin");
+
             Console.WriteLine("Hello World!");
         }
     }
