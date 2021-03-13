363a37d1cf1b7888d6a29742b6c6a98a9f99a03c
Yakup İlyas Çağan <yakupilyascagan@gmail.com>
2021-03-13 12:03:21 +0300

    Add project files.

---
+++ KampDevam/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KampDevam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
