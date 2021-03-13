869f8b5751ac6dcd344a3b6b915d41cee205a3a0
Yakup İlyas Çağan <yakupilyascagan@gmail.com>
2021-03-13 14:56:07 +0300

    13 mart 2021 kampadevam

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName+" eklendi");
+        }
+
+        public void Uptade(Product product)
+        {
+            Console.WriteLine(product.ProductName+" güncellendi.");
+        }
+
+       
+
+
+
+    }
+}
