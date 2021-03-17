ef29cb823d3167b716810e467298b51a9774c9cd
Yakup İlyas Çağan <yakupilyascagan@gmail.com>
2021-03-17 23:37:39 +0300

    17/03/2021 Dictionary İlk kullanım Örnek

---
+++ DictionaryGirisOrnek/MyDictionary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryGirisOrnek
+{
+    class MyDictionary<Tkey,Tvalue>
+    {
+        Tkey[] arrayKey;
+        Tvalue[] arrayValue;
+
+        public MyDictionary()
+        {
+            arrayKey = new Tkey[0];
+            arrayValue = new Tvalue[0];
+        }
+
+        public void Add(Tvalue müsteri, Tkey numara)
+        {
+            Tkey[] tempKey = arrayKey;
+            arrayKey = new Tkey[tempKey.Length + 1];
+
+            Tvalue[] tempValue = arrayValue;
+            arrayValue = new Tvalue[tempValue.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                arrayKey[i] = tempKey[i];
+                arrayValue[i] = tempValue[i];
+            }
+            arrayKey[arrayKey.Length - 1] = numara;
+            arrayValue[arrayValue.Length - 1] = müsteri;
+        }
+
+        public int Count
+        {
+            get { return arrayKey.Length; }
+        }
+
+    }
+}
---
+++ DictionaryGirisOrnek/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DictionaryGirisOrnek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> müsteriler = new MyDictionary<string, int>();
+            müsteriler.Add(6,"Yunus Emre");
+            Console.WriteLine(müsteriler.Count);
+            
+            //1
+
+            müsteriler.Add(7, "Halil İbrahim");
+            Console.WriteLine(müsteriler.Count);
+
+            müsteriler.Add(8, "Habibe Nur");
+            Console.WriteLine(müsteriler.Count);
+
+
+
+        }
+    }
+}
